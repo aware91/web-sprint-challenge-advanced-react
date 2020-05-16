4d01c508a4a41982d8c774a862f69f026188fb87
Anthony Ware <aware91coding@gmail.com>
2020-05-15 21:38:23 -0500

    got the first part of form message to test correct

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
 
-test("form shows success message on submit with form details", () => {});
+});
+
+test("form shows success message on submit with form details", () => {
+    const { getByTestId } = render(<CheckoutForm />);
+    const expectedFirstName = "Anthony";
+    const expectedLastName = "Ware";
+    const expectedAddress = "123 Road Rd";
+    const expectedCity = "Joplin";
+    const expectedState = "MO";
+    const expectedZip = "64834";
+    const firstName = getByTestId('firstName');
+    const lastName = getByTestId('lastName');
+    const address = getByTestId('address');
+    const city = getByTestId('city');
+    const state = getByTestId('state');
+    const zip = getByTestId('zip');
+    fireEvent.change(firstName, {target: {value: 'Anthony'}});
+    fireEvent.change(lastName, {target: {value: 'Ware'}});
+    fireEvent.change(address, {target: {value: '123 Road Rd'}});
+    fireEvent.change(city, {target: {value: 'Joplin'}});
+    fireEvent.change(state, {target: {value: 'MO'}});
+    fireEvent.change(zip, {target: {value: '64834'}});
+    expect(firstName.value).toBe(expectedFirstName);
+    expect(lastName.value).toBe(expectedLastName);
+    expect(address.value).toBe(expectedAddress);
+    expect(city.value).toBe(expectedCity);
+    expect(state.value).toBe(expectedState);
+    expect(zip.value).toBe(expectedZip);
+});
